8b846eff6e430c3318c65cd4d2c8927f1aa4b8ef
matty1504 <matty1504@korea.ac.kr>
2024-02-05 00:00:54 +0900

    Changes to be committed: 	modified:   find.py

--- translate/find.py
+++ translate/find.py
@@ -138,12 +138,12 @@ def makespace(biglist):
     blank = [0,0,0,0]
 
     while(len(biglist)>1) :
-        print(len(biglist))
+        #print(len(biglist))
         firstlist=biglist.pop(0)
         secondlist=biglist[0]
         a=checkf(firstlist)
         b=checkf(secondlist)
-        print (a, b)
+        #print (a, b)
         if a==4 or b==4 : #앞이나 뒤가 공백이면
             outlist.append(firstlist)
         elif a==3 or b==3: #앞이나 뒤가 숫자이면
@@ -254,40 +254,44 @@ divide = []
 for i in range(3):
 # 첫번째 2차원배열에 만들기
     value=input()
-    if value=="000000":
-        nmode=0
-        letter.append([0,0,0,0])
-        letter.append([0,0,0,0])
+    if value=="001001":
+         letter.append([0,0,0,0])
+         #숫자모드에서 001001은 '-' 그래서 이거에 관한 처리하려면 이 경우엔 숫자칸에 -에 해당하는거 만들어서 넣어줘야함
     else:
-        if nmode==0: 
-            if value == "001111":
-                nmode=1
-            else:
-                matval=match(value)
-                if len(matval)==2:
-                    #우애넣었을때 여기서 에러가 남
-                    letter.append(makecol(matval[0],matval[1]))
-                else:
-                    #약어인 경우
-                    letter.append(makecol(matval[0],matval[1]))
-                    letter.append(makecol(matval[2],matval[3]))
+        if value=="000000":
+            nmode=0
+            letter.append([0,0,0,0])
+            letter.append([0,0,0,0])
         else:
-            matval=dicnum.get(value)
-            #숫자에서 찾음
-            if matval == None:
-                nmode=0
-                #숫자에서 나와서 다시 찾아 넣기
-                matval=match(value)
-                if len(matval)==2:
-                    letter.append(makecol(matval[0],matval[1]))
+            if nmode==0: 
+                if value == "001111":
+                    nmode=1
                 else:
-                    #약어인 경우
-                    letter.append(makecol(matval[0],matval[1]))
-                    letter.append(makecol(matval[2],matval[3]))
+                    matval=match(value)
+                    if len(matval)==2:
+                        #우애넣었을때 여기서 에러가 남
+                        letter.append(makecol(matval[0],matval[1]))
+                    else:
+                        #약어인 경우
+                        letter.append(makecol(matval[0],matval[1]))
+                        letter.append(makecol(matval[2],matval[3]))
             else:
-                matval = (3,matval)
-                letter.append(makecol(matval[0],matval[1]))
-                # 찾은 숫자열 2차원배열에 넣기
+                matval=dicnum.get(value)
+                #숫자에서 찾음
+                if matval == None:
+                    nmode=0
+                    #숫자에서 나와서 다시 찾아 넣기
+                    matval=match(value)
+                    if len(matval)==2:
+                        letter.append(makecol(matval[0],matval[1]))
+                    else:
+                        #약어인 경우
+                        letter.append(makecol(matval[0],matval[1]))
+                        letter.append(makecol(matval[2],matval[3]))
+                else:
+                    matval = (3,matval)
+                    letter.append(makecol(matval[0],matval[1]))
+                    # 찾은 숫자열 2차원배열에 넣기
 #공백만들기
 divide =makespace(letter)
 
