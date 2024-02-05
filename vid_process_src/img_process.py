f12f726ea540e06f2ae23c3928dda90c343b6103
she743 <she743@naver.com>
2024-02-05 01:10:21 -0600

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   img_process.py 	modified:   img_process2.py 	new file:   img_process3.py

--- vid_process_src/img_process.py
+++ vid_process_src/img_process.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-src = cv2.imread('/home/rasp/Desktop/HandS_Braille/captured_img/test_cap_img2.jpg', cv2.IMREAD_COLOR)
+src = cv2.imread('/home/rasp/Desktop/HandS_Braille/vid_process_src/captured_img/test_cap_img2.jpg', cv2.IMREAD_COLOR)
 src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
 img_gaussian1 = cv2.GaussianBlur(src_gray, (3, 3), 0)
@@ -29,7 +29,7 @@ for y in range(4):
     for x in range(4):
         src_ = img_median[y*bh:(y+1)*bh, x*bw:(x+1)*bw]
         dst_ = dst1[y*bh:(y+1)*bh, x*bw:(x+1)*bw]
-        cv2.threshold(src_, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU, dst_)
+        cv2.threshold(src_, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU, dst_)
 
 
 # 영상 안에 있는 흰색 덩어리를 정수 형태로 리턴
@@ -37,13 +37,13 @@ cnt1, _ = cv2.connectedComponents(dst1)
 print('cnt1:', cnt1)
 
 # 모폴로지 열기
-dst2 = cv2.morphologyEx(dst1, cv2.MORPH_OPEN, None)
+dst2 = cv2.morphologyEx(dst1, cv2.MORPH_GRADIENT, None)
 
 # 영상 안에 있는 흰색 덩어리를 정수 형태로 리턴
 cnt2, _ = cv2.connectedComponents(dst2)
 print('cnt2:', cnt2)
 
-# cimg = cv2.cvtColor(img_median, cv2.COLOR_GRAY2BGR)
+cimg = cv2.cvtColor(img_median, cv2.COLOR_GRAY2BGR)
 
 circles = cv2.HoughCircles(img_median, cv2.HOUGH_GRADIENT, 1, 30, param1=250, param2=11, minRadius=5, maxRadius=12)
 
@@ -52,12 +52,12 @@ circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
     cv2.circle(img_median,(int(i[0]),int(i[1])), 18, (0,255,255), 5)
 
-# cv2.imshow('dst1', dst1)
-# cv2.imshow('dst2', dst2)
+cv2.imshow('dst1', dst1)
+cv2.imshow('dst2', dst2)
 
 # cv2.imshow('gaussiangray', img_grayscale_gaussian1)
 # cv2.imshow('adaptivegray2', img_grayscale)
-cv2.imshow('median', img_median)
+# cv2.imshow('median', img_median)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
